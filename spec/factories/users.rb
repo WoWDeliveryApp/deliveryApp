39ce5d0a7bf4333b178331d59554c3a145eb74f5
mtorres8969 <loldlm1@gmail.com>
2017-02-02 11:13:17 -0400

    configurado Rspec y tests para User Model

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,7 @@
 FactoryGirl.define do
   factory :user do
-    
+    name 'bob'
+    email { Faker::Internet.email }
+    password 'please123'
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { FactoryGirl.create(:user) }
+
+  it { is_expected.to respond_to(:name) }
+  it { is_expected.to respond_to(:address) }
+  
+  it 'set the api_token' do
+    expect(user.api_token).to be_present
+  end
+  
+  describe 'setting roles' do
+    before do
+      user
+    end
+
+    it 'customer is the default role' do
+      expect(user.role).to eq 'customer'
+    end
+
+    it 'set role to deliveryman' do
+      user.update(role: 1)
+      user.reload
+      expect(user.role).to eq 'deliveryman'
+    end
+  end
 end
