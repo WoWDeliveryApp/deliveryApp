e6529531e6340eca324d71221c6d93b458e81766
mtorres8969 <loldlm1@gmail.com>
2017-02-20 16:46:03 -0400

    Ya podemos colocarle el modal al registro de usuario :)

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -9,7 +9,13 @@ before_action :configure_account_update_params, only: [:update]
 
   # POST /resource
   def create
-    super
+    @user = build_resource(configure_sign_up_params)
+    respond_to do |format|
+      if @user.save
+        sign_in(@user)
+      end
+      format.js
+    end
   end
 
   # GET /resource/edit
@@ -40,7 +46,11 @@ before_action :configure_account_update_params, only: [:update]
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute, :name, :address])
+    params.require(:user).permit(:name, 
+      :address,
+      :email,
+      :password,
+      :password_confirmation)
   end
 
   # If you have extra params to permit, append them to the sanitizer.
