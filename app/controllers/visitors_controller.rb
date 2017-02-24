71cb4bcbabd862f8b29faaf623f930e8013a81c1
mtorres8969 <loldlm1@gmail.com>
2017-02-24 11:44:01 -0400

    Multi-idioma working pero falta las traducciones xd

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,4 +1,13 @@
 class VisitorsController < ApplicationController
   def index
   end
+
+  def set_locale
+    begin 
+      I18n.locale = params[:locale].to_sym
+      redirect_to :back
+    rescue NoMethodError, ActionController::RedirectBackError
+      redirect_to root_path
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  
   root to: 'visitors#index'
+  post '/locale' => 'visitors#set_locale', as: 'locale'
 
   devise_for :users, controllers: { registrations: 'registrations', sessions: 'sessions', passwords: 'passwords' }
-
 end
